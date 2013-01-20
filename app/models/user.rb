067c8b7d0467d08aa1978826d3342b5e3aa68f42
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-01-20 14:06:28 +0100

    Fix validation for pencilbox address and add validation message

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   has_one :box
 
-  validates :username, presence: true
+  validates :username, presence: true, format: { with: /\A[-a-z0-9_]+\Z/i, message: "is invalid. You can use letters (a-z), numbers (0-9), underscores (_) and dashes (-)." }
 
   # Setup accessible (or protected) attributes for your model
   # Move this to strong_params
