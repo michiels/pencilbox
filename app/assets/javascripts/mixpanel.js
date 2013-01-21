e77860964e0ef5b9685768d28af9e036d8b3a99b
Bob Jansen <bob@firmhouse.com>
2013-01-21 21:01:11 +0100

    Added javascript link tracking for registration and log in forms

--- app/assets/javascripts/mixpanel.js
+++ app/assets/javascripts/mixpanel.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
   mixpanel.track_links('a#dropbox-signup-frontpage', "Starts dropbox link", {'Page': 'Frontpage'})
+  mixpanel.track_forms('form#user_registers', "User registers")
+  mixpanel.track_forms('form#new_user', "User sign-in")
 });
