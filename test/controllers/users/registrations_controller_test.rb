3d34207fea407884a8ce5d26b02bf53aefe4a9ba
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-01-21 11:30:31 +0100

    Test for redirection to correct user box url on registration

--- test/controllers/users/registrations_controller_test.rb
+++ test/controllers/users/registrations_controller_test.rb
@@ -7,7 +7,7 @@ class Users::RegistrationsControllerTest < ActionController::TestCase
 
     post :create, user: { username: "newuser", email: "newuser@newdomain.com", password: "password"}
 
-    assert_redirected_to box_url(boxes(:no_user))
+    assert_redirected_to box_url(id: boxes(:no_user).reload.user.username)
     assert_equal "newuser", boxes(:no_user).reload.user.username
 
     assert_nil session['devise.signup_box_id']
