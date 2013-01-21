d63b5b2002890a3a508deecb8a6c0b5a94a5198e
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-01-22 00:38:16 +0100

    Set a nicer (but not nice enough yet) page title for articles and folders

--- app/helpers/boxes_helper.rb
+++ app/helpers/boxes_helper.rb
@@ -1,10 +1,12 @@
 module BoxesHelper
 
   def box_page_title(box)
-    if box.present?
+    if @page_title.present?
+      @page_title
+    elsif box.present?
       "This is my Pencilbox &mdash; #{sanitize(@box.display_name)}".html_safe
     else
-      @title
+      "Not found!"
     end
   end
 end
